82652911d07d9e65ad658129ec279c405985c304
Matej Dujič <matej.dujic@visionect.com>
2018-03-28 15:58:15 +0200

    dnevnik in izracuni

--- print.py
+++ print.py
@@ -1,1 +1,10 @@
-print("bla")
+masa = 70
+numofbeers = 10
+strength = 0.05
+blood_volume = 5000
+
+total_alko = numofbeers * strength * 1000
+print (total_alko)
+
+percent_alko = total_alko / blood_volume * 100
+print(percent_alko)
