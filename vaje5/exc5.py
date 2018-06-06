3ffb2f67a211196425360a1de520effd924abb87
Matej Dujič <matej.dujic@visionect.com>
2018-06-06 10:25:28 +0200

    exc5 solution

--- vaje5/exc5.py
+++ vaje5/exc5.py
@@ -1,3 +1,5 @@
+from random import random
+
 # Instructions
 
 # Create a program that represents a football bet.
@@ -19,3 +21,41 @@
 # ....
 # --------------
 # You earned -5€.
+
+
+list_home = ["Liverpool", "Bate Borisov", "Olimpija", "Milan", "Barcelona"]
+list_away = ["CSKA", "Partizan", "Juventus", "Bayern", "PSV"]
+user_results = []
+
+
+def bet():
+    print("Input your forecast, maximum goal by one team is 5")
+    for i, v in enumerate(list_home):
+        success = False
+        while not success:
+            inp = input("{} : {} ?\n".format(v, list_away[i]))
+            if len(inp) < 3:
+                print("Invalid input")
+            else:
+                user_results.append(inp)
+                success = True
+
+
+def match_generator():
+    return str(int(random() * 5) + 1), str(int(random() * 5) + 1)
+
+
+bet()
+earnings = 0
+print("\t\t[Bet results]...")
+for ind, v in enumerate(user_results):
+    home, away = match_generator()
+    print("Actual game result:\n {} {} : {} {}".format(list_home[ind], home, away, list_away[ind]))
+    if v[0] == home and away in v[0:]:
+        print("BRAVO, YOU EARNED 10€ !!!")
+        earnings += 10
+    else:
+        print("Your bet:\n {} {} : {} {}".format(list_home[ind], v[0], v[2], list_away[ind]) + "\n---------------------")
+        earnings -= 1
+print("Money earned: {}€".format(earnings))
+
